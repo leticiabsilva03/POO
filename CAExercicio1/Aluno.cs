b464f4ff0e7e72d7823dddb6d7ff599c2e9e74b3
leticiabsilva03 <leticiabsilva03@gmail.com>
2021-08-06 16:25:00 -0300

    Add files via upload
    Exercício 1 Lista de POO

---
+++ CAExercicio1/Aluno.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAExercicio1
+{
+    class Aluno
+    {
+        // atributos da classe
+        private String m_nome;
+        private String m_matricula;
+        private String m_telefone;
+
+        // construtor padrão
+        public Aluno()
+        {
+            m_nome = "";
+            m_matricula = "";
+            m_telefone = "";
+        }
+
+        // construtor com argumentos
+        public Aluno(String nome, String matricula, String telefone)
+        {
+            m_nome = nome;
+            m_matricula = matricula;
+            m_telefone = telefone;
+        }
+
+        // getters
+        public String getNome()
+        {
+            return (m_nome);
+        }
+
+        public String getMatricula()
+        {
+            return (m_matricula);
+        }
+
+        public String getTelefone()
+        {
+            return (m_telefone);
+        }
+
+        // setters
+        public void setNome(String nome)
+        {
+            m_nome = nome;
+        }
+
+        public void setMatricula(String matricula)
+        {
+            m_matricula = matricula;
+        }
+
+        public void setTelefone(String telefone)
+        {
+            m_telefone = telefone;
+        }
+
+        public void imprimirDados()
+        {
+            Console.WriteLine("\t\t\t" + "Dados do Aluno");
+            Console.WriteLine("\nNome: " + m_nome);
+            Console.WriteLine("Número de Matrícula: " + m_matricula);
+            Console.WriteLine("Número do Telefone: " + m_telefone);
+        }
+    }
+}
---
+++ CAExercicio1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CAExercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno1 = new Aluno("Leticia", "12011EEL028", "92915341");
+            Aluno aluno2 = new Aluno("", "", "");
+
+            // Mostrando valores iniciados pelo usuário
+            Console.WriteLine("\nContrutor com valores iniciados pelo usuário: \n ");
+            Console.WriteLine("Nome do Aluno: " + aluno1.getNome());
+            Console.WriteLine("Matrícula do Aluno: " + aluno1.getMatricula());
+            Console.WriteLine("Telefone do Aluno: " + aluno1.getTelefone());
+
+            // Lendo valores do usuário
+            Console.WriteLine("\n\nInforme o nome do aluno: ");
+            aluno2.setNome(Console.ReadLine()); ;
+            Console.WriteLine("Informe a matrícula do aluno: ");
+            aluno2.setMatricula(Console.ReadLine());
+            Console.WriteLine("Informe o telefone do aluno: ");
+            aluno2.setTelefone(Console.ReadLine());
+
+
+            Console.WriteLine("\nDados do aluno informados pelo usuário \n");
+            Console.WriteLine("Nome: " + aluno2.getNome());
+            Console.WriteLine("Matrícula: " + aluno2.getMatricula());
+            Console.WriteLine("Telefone: " + aluno2.getTelefone());
+
+            Console.ReadLine();
+        }
+    }
+}
