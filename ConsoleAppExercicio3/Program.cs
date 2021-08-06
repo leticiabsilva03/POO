30cf9e42ec0bc4417204c22e4a9230615122b0f9
leticiabsilva03 <leticiabsilva03@gmail.com>
2021-08-06 15:51:05 -0300

    Add files via upload
    Exercício 3 da lista de POO - Letras A e B

---
+++ ConsoleAppExercicio3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleAppExercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tempo h1 = new Tempo();
+            Tempo h2 = new Tempo(14, 35, 43);
+            Tempo h3 = new Tempo(0, 0, 0);
+
+            // Iniciando com valores vazios
+            Console.WriteLine("Contrutor Padrão Vazio: \n "); 
+            Console.WriteLine("Horas: " + h2.t_hora + ":" + h2.t_min + ":" + h2.t_segs);
+           
+
+            // Lendo valores do usuário
+            Console.WriteLine("\n\nInforme a hora: ");
+            h3.setHora(int.Parse(Console.ReadLine())); // parse converte string representation of a number to its 32-bit signed integer equivalent
+            Console.WriteLine("Informe os minutos: ");
+            h3.setMinutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Informe os segundos: ");
+            h3.setSegundos(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("\nHora informada pelo usuário: " + h3.getHora() + ":" + h3.getMinutos() + ":" + h3.getSegundos());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleAppExercicio3/Tempo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppExercicio3
+{
+    class Tempo
+    {
+        // atributos da classe
+        public int t_hora;
+        public int t_min;
+        public int t_segs;
+
+        // construtor padrão
+
+        public Tempo()
+        {
+            t_hora = 00;
+            t_min = 00;
+            t_segs = 00;
+        }
+
+        // construtor que inicialize os dados pelo teclado
+        public Tempo (int hora, int min, int segs)
+        {
+            t_hora = hora;
+            t_min = min;
+            t_segs = segs;
+        }
+
+        // método para imprimir hh:mm:ss
+        public int getHora()
+        {
+            return t_hora;
+        }
+        public int getMinutos()
+        {
+            return t_min;
+        }
+        public int getSegundos()
+        {
+            return t_segs;
+        }
+        public void setHora(int hora)
+        {
+            t_hora = hora;
+        }
+        public void setMinutos(int min)
+        {
+            t_min = min;
+        }
+        public void setSegundos(int segs)
+        {
+            t_segs = segs;
+        }
+        public int Tempo_Hora()
+        {
+            return (t_hora);
+        }
+        public int Tempo_Minutos()
+        {
+            return (t_min);
+        }
+        public int Tempo_Segundos()
+        {
+            return (t_segs);
+        }
+    }
+}
