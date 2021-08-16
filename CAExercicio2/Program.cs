3ab3cb0fd732262f610d590e8498c4b81f3b9a8c
leticiabsilva03 <leticiabsilva03@gmail.com>
2021-08-16 16:02:59 -0300

    Add files via upload

---
+++ CAExercicio2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CAExercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Construtor Padrão
+            Vetor2D V1 = new Vetor2D();
+
+            Console.WriteLine("Construtor padrão: ");
+            Console.WriteLine("Vetor: " + V1.getX() + ", " + V1.getY() + ") \n");
+
+            // Construtor com argumentos 
+            Vetor2D V2 = new Vetor2D(2.0, 1.0);
+
+            Console.WriteLine("Construtor com argumentos");
+            Console.WriteLine("Vetor: (" + V2.getX() + ", " + V2.getY() + ") \n");
+
+            // Lendo valores do console
+            Console.Write("Entre com a coordenada X: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Entre com a coordenada Y: ");
+            double y = double.Parse(Console.ReadLine());
+            Vetor2D V3 = new Vetor2D(x, y);
+            Console.WriteLine("\nVetor informado pelo usuario: (" + V3.getX() + ", " + V3.getY() + ") \n");
+
+
+            // Apresentando modulo do vetor
+            Console.WriteLine("Módulo de (" + V3.getX() + "," + V3.getY() + "): " + V3.modulo() + " \n");
+
+            // Apresentando angulo do vetor
+            Console.WriteLine("Angulo entre (" + V2.getX() + "," + V2.getY() + ") e (" + V3.getX() + "," + V3.getY() + "): " + V2.angulo(V3) + "°\n");
+
+            // Apresentando produto escalar entre os vetores
+            Console.WriteLine("Produto escalar entre (" + V2.getX() + "," + V2.getY() + ") e (" + V3.getX() + "," + V3.getY() + "): " + V2.produtoEscalar(V3) + "\n");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CAExercicio2/Vetor2D.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAExercicio2
+{
+    class Vetor2D
+    {
+        // atributos da classe
+        private double m_x;
+        private double m_y;
+        double m_modulo;
+        double m_angulo;
+
+        // contrutores
+
+        // construtor default para criar vetor nulo
+        public Vetor2D()
+        {
+            m_x = 0.0;
+            m_y = 0.0;
+        }
+
+        // construtor que receberá valores de x e y
+        public Vetor2D(double x, double y)
+        {
+            m_x = x;
+            m_y = y;
+        }
+
+        // getters
+        public double getX()
+        {
+            return (m_x);
+        }
+        public double getY()
+        {
+            return (m_y);
+        }
+
+        // setters
+        public void setX(double x)
+        {
+            m_x = x;
+        }
+        public void setY(double y)
+        {
+            m_y = y;
+        }
+
+        // função membro para retornar módulo do vetor
+        public double modulo()
+        {
+            m_modulo = Math.Sqrt(Math.Pow(m_x, 2) + Math.Pow(m_y, 2));
+            return (m_modulo);
+        }
+
+        // função produto escalar entre dois vetores 2D
+        public double produtoEscalar(Vetor2D V)
+        {
+            double produto_escalar = (m_x * V.m_x) + (m_y * V.m_y);
+            return (produto_escalar);
+        }
+
+        // função angulo entre dois vetores 2D
+        public double angulo(Vetor2D V)
+        {
+            m_angulo = Math.Acos(produtoEscalar(V)/ Math.Sqrt(Math.Pow(m_x, 2) + Math.Pow(m_y, 2))*V.modulo());
+            m_angulo = m_angulo * (180 / Math.PI);
+            return (m_angulo);
+        }
+
+    }
+}
