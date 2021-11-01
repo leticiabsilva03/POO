d9906a464e801b06653dbf4d48dc5133bc755440
leticiabsilva03 <leticiabsilva03@gmail.com>
2021-11-01 11:56:12 -0300

    Add files via upload
    Questão 1 Prova 2 POO

---
+++ wfaContainers/NohPilha.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wfaContainers
+{
+    class NohPilha
+    {
+        // atributos
+        private int data;
+        private NohPilha nextNoh;
+
+        // construtor padrão
+        public NohPilha()
+        {
+            data = 0;
+            nextNoh = null;
+        }
+
+        // construtor com argumentos - aridade 1
+        public NohPilha(int valor)
+        {
+            data = valor;
+            nextNoh = null;
+        }
+
+        // construtor com argumentos - aridade 2
+        public NohPilha(int valor, NohPilha noh)
+        {
+            data = valor;
+            nextNoh = noh;
+        }
+
+        public int getData()
+        {
+            return (data);
+        }
+
+        public void setData(int valor)
+        {
+            data = valor;
+        }
+
+        public NohPilha getNext()
+        {
+            return (nextNoh);
+        }
+
+        public void setNext(NohPilha newNoh)
+        {
+            nextNoh = newNoh;
+        }
+
+    }
+}
